c33ae2a7ec1325ce5e632036a6cacf2b34bdec9e
ohughes16 <ohughes16@gmail.com>
2020-11-17 22:48:07 -0800

    Finished module 10.6.1 but am having an internal server error

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import scraping
 app = Flask(__name__)
 
 #Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017//mars_app"
+app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017/mars_app"
 mongo = PyMongo(app)
 
 @app.route("/")
